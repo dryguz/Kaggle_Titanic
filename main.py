9cae760aaea9d405f69c7be9b08c4d484c80df73
dryguz <dryguz@gmail.com>
2018-03-19 21:28:27 +0100

    Finished log_reg, started random_forest

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Created on Tue Jan 30 14:40:59 2018
 """
 import sys, os
 import numpy as np
+import time
 path = '/home/mdrygajlo/PycharmProjects/Kaggle_Titanic/'
 sys.path.append(path)
 os.chdir(path)
@@ -25,16 +26,37 @@ X, y = cleaner.fit(train)
 models = {'svm': [], 'log_reg': [], 'rand_forest': [], 'xgboost': []}
 
 # first one - svm
+start = time.time()
 svm_acc, svm_model = modeling.svm_(X, y)
+duration = time.time() - start
+
 models['svm'].append(np.mean(svm_acc))
+models['svm'].append(duration)
 models['svm'].append(svm_model)
 
 print('Score for SVM model is {:.3}'.format(models['svm'][0]))
 
 # second one - logical regression
-lg_acc, lg_model = modeling.lg_(X,y)
+start = time.time()
+lg_acc, lg_model = modeling.log_reg_(X,y)
+duration = time.time() - start
+
 models['log_reg'].append(np.mean(lg_acc))
+models['log_reg'].append(duration)
 models['log_reg'].append(lg_model)
 
 print('Score for Logical Regression is {:.3}'.format(models['log_reg'][0]))
+
+#third one - random forest
+start = time.time()
+rf_acc, rf_model = modeling.rand_forest_(X,y)
+duration = time.time() - start
+
+models['rand_forest_'].append(np.mean(rf_acc))
+models['rand_forest_'].append(duration)
+models['rand_forest_'].append(rf_model)
+
+print('Score for Random Forest is {:.3}'.format(models['rand_forest_'][0]))
+
+
 
--- modeling.py
+++ modeling.py
@@ -8,6 +8,7 @@ create classifications model
 """
 
 from sklearn import svm, linear_model
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV, cross_val_score
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline, FeatureUnion 
@@ -36,7 +37,8 @@ def svm_(X, y):
     def select_not_to_scal(data):
         return data[:,not_for_scalling]
     
-    #cls = svm.SVC(kernel = 'rbf')
+    svm_cls = svm.SVC(kernel = 'rbf',
+                      random_state=123)
     
     steps = [('Union', FeatureUnion(transformer_list = [
                 ('for_scaler', Pipeline(steps =[
@@ -49,7 +51,7 @@ def svm_(X, y):
                 ('not_scaler', FunctionTransformer(select_not_to_scal))
                 ])
             ),
-            ('Estimator', svm.SVC(kernel = 'rbf'))
+            ('Estimator', svm_cls)
             ]
     
     ppl = Pipeline(steps=steps)
@@ -59,7 +61,8 @@ def svm_(X, y):
     
     rm_clf = RandomizedSearchCV(ppl, 
                                 param_distributions=parameters, 
-                                n_iter=20,
+                                n_iter=30,
+                                random_state=123,                                
                                 cv=5)
 
     svm_model = rm_clf.fit(X, y)
@@ -72,28 +75,115 @@ def svm_(X, y):
 # ---------------------------------------------------------------------------
 # log_reg_model
     
-def log_reg_(df):
+def log_reg_(X, y):
+    
+    for_scalling =[]
+    
+    for col in ['Age', 'SibSp', 'Parch', 'Fare']:
+        for_scalling.append(X.columns.get_loc(col))
+        
+    not_for_scalling = list(set(range(len(X.columns)))-set(for_scalling))
+    
+    
+    def select_to_scal(data):
+        return data[:,for_scalling]
+    
+    
+    def select_not_to_scal(data):
+        return data[:,not_for_scalling]
+    
     
     sgd_cls = linear_model.SGDClassifier(loss='log', 
                                          penalty='elasticnet',
+                                         tol=0.001,
                                          random_state=123)
     
-    n_iter = np.ceil(10**6 / n)
     
-    parameters = {'alpha': np.linspace(0.0001, 0.1, 50),
-                  'l1_ratio': np.linspace(0, 1, 50),
-                  'eta0': np.linspace(0,0.999, 50),
-                  
-            }
+    steps = [('Union', FeatureUnion(transformer_list = [
+            ('for_scaler', Pipeline(steps =[
+                    ('select_for_scaler', FunctionTransformer(
+                            select_to_scal)
+                            ),
+                    ('StandardScaler', StandardScaler()
+                            )])
+            ),
+            ('not_scaler', FunctionTransformer(select_not_to_scal))
+            ])
+        ),
+        ('Estimator', sgd_cls)
+        ]
     
-    log_reg_acc, log_reg_model = 1, 1
+    ppl = Pipeline(steps=steps)
+    
+    
+    parameters = {'Estimator__alpha': np.linspace(0.0001, 0.1, 50),
+                  'Estimator__l1_ratio': np.linspace(0, 1, 50)
+                  }
+    
+    rm_clf = RandomizedSearchCV(ppl, 
+                                param_distributions=parameters, 
+                                n_iter=30,
+                                random_state=123,
+                                cv=5)
+    
+    
+    log_reg_model = rm_clf.fit(X, y)
+    log_reg_acc  =  cross_val_score(log_reg_model.best_estimator_, X, y, cv=5)
+    
     return log_reg_acc, log_reg_model
 
 
 # ---------------------------------------------------------------------------
 # rand_forest_model
     
-def rand_forest_(df):
+def rand_forest_(X, y):
+    
+    for_scalling =[]
+    
+    for col in ['Age', 'SibSp', 'Parch', 'Fare']:
+        for_scalling.append(X.columns.get_loc(col))
+        
+    not_for_scalling = list(set(range(len(X.columns)))-set(for_scalling))
+    
+    
+    def select_to_scal(data):
+        return data[:,for_scalling]
+    
+    def select_not_to_scal(data):
+        return data[:,not_for_scalling]
+    
+    
+    rf_clf = RandomForestClassifier(random_state=123
+                                    
+                                    )
+
+    steps = [('Union', FeatureUnion(transformer_list = [
+            ('for_scaler', Pipeline(steps =[
+                    ('select_for_scaler', FunctionTransformer(
+                            select_to_scal)
+                            ),
+                    ('StandardScaler', StandardScaler()
+                            )])
+            ),
+            ('not_scaler', FunctionTransformer(select_not_to_scal))
+            ])
+        ),
+        ('Estimator', rf_clf)
+        ]
+    
+    ppl = Pipeline(steps=steps)
+
+
+    parameters = {'Estimator__n_estimators': np.linspace(3, 100, 3),
+                  'Estimator__': '',
+                  'Estimator__': ''
+            }
+    
+    rm_clf = RandomizedSearchCV(ppl, 
+                                param_distributions=parameters, 
+                                n_iter=30,
+                                random_state=123,
+                                cv=5)
     
     rand_forest_model, rand_forest_model = 1, 1
     return rand_forest_model, rand_forest_model
