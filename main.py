c28d857b87e3d0826d95c0976c3906bd0d482141
dryguz <dryguz@gmail.com>
2018-03-24 21:30:54 +0100

    rewritten modeling module, using single class, tested on svm - its working

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ Created on Tue Jan 30 14:40:59 2018
 import sys, os
 import numpy as np
 import time
+from sklearn import svm
+
 path = '/home/mdrygajlo/PycharmProjects/Kaggle_Titanic/'
 sys.path.append(path)
 os.chdir(path)
@@ -23,7 +25,11 @@ train, test = import_data()
 X, y = cleaner.fit(train)
 
 # set dictionary with models
-models = {'svm': [], 'log_reg': [], 'rand_forest': [], 'xgboost': []}
+models = {'svm': [],
+          'svm_class': [],
+          'log_reg': [], 
+          'rand_forest': [], 
+          'xgboost': []}
 
 # first one - svm
 start = time.time()
@@ -48,15 +54,36 @@ models['log_reg'].append(lg_model)
 print('Score for Logical Regression is {:.3}'.format(models['log_reg'][0]))
 
 #third one - random forest
+#start = time.time()
+#rf_acc, rf_model = modeling.rand_forest_(X,y)
+#duration = time.time() - start
+
+#models['rand_forest_'].append(np.mean(rf_acc))
+#models['rand_forest_'].append(duration)
+#models['rand_forest_'].append(rf_model)
+
+#print('Score for Random Forest is {:.3}'.format(models['rand_forest_'][0]))
+
+# ----------------------------------------------------------------------------
+# test svm with class
+
+svm_parameters = {'Estimator__C': np.linspace(1, 1000, num=50),
+              'Estimator__gamma': np.linspace(0.0001, 0.1, num=50)}
+
+svm_model = svm.SVC(kernel = 'rbf',
+                  random_state=123)
+        
+_svm_ = modeling.model_(svm_model, svm_parameters)
+
 start = time.time()
-rf_acc, rf_model = modeling.rand_forest_(X,y)
+
+_svm_model = _svm_.fit(X, y)
+_svm_acc = _svm_.score(X, y)
+
 duration = time.time() - start
 
-models['rand_forest_'].append(np.mean(rf_acc))
-models['rand_forest_'].append(duration)
-models['rand_forest_'].append(rf_model)
+models['svm_class'].append(np.mean(_svm_acc))
+models['svm_class'].append(duration)
+models['svm_class'].append(_svm_model)
 
-print('Score for Random Forest is {:.3}'.format(models['rand_forest_'][0]))
-
-
-
+print('Score for SVM model is {:.3}'.format(models['svm_class'][0]))
--- modeling.py
+++ modeling.py
@@ -14,6 +14,79 @@ from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline, FeatureUnion 
 import numpy as np
 import pandas as pd
+
+# ---------------------------------------------------------------------------
+# class for models, with training method, predict method
+
+
+class model_:
+    def __init__(self, model, parameters_for_model):
+        self.model = model
+        self.parameters = parameters_for_model    
+        self.for_scalling =[]
+        self.X = pd.DataFrame()
+        self.y = pd.DataFrame()
+        
+
+        
+        
+        def select_to_scal(data):
+            return data[:,self.for_scalling]
+        
+        
+        def select_not_to_scal(data):
+            return data[:,self.not_for_scalling]
+        
+        
+        steps = [('Union', FeatureUnion(transformer_list = [
+                    ('for_scaler', Pipeline(steps =[
+                            ('select_for_scaler', FunctionTransformer(
+                                    select_to_scal)
+                                    ),
+                            ('StandardScaler', StandardScaler()
+                                    )])
+                    ),
+                    ('not_scaler', FunctionTransformer(select_not_to_scal))
+                    ])
+                ),
+                ('Estimator', self.model)
+                ]
+        
+        self.ppl = Pipeline(steps=steps)
+        
+        self.clf = RandomizedSearchCV(self.ppl, 
+                        param_distributions=self.parameters, 
+                        n_iter=30,
+                        random_state=123,                                
+                        cv=5)
+    
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        
+        for col in ['Age', 'SibSp', 'Parch', 'Fare']:
+            self.for_scalling.append(self.X.columns.get_loc(col))
+            
+        self.not_for_scalling = list(set(range(len(self.X.columns)))-set(self.for_scalling))
+        
+        self.trained_model = self.clf.fit(self.X,self.y)
+        
+        return self.trained_model
+    
+    def score(self, X, y):
+        self.X = X
+        self.y = y
+        
+        
+        
+        self.models_score = cross_val_score(
+                self.trained_model.best_estimator_, 
+                self.X, 
+                self.y, cv=5)
+        return self.models_score
+ 
+
+    
 # ---------------------------------------------------------------------------
 # svn_model
 
@@ -179,7 +252,7 @@ def rand_forest_(X, y):
                   'Estimator__': ''
             }
     
-    rm_clf = RandomizedSearchCV(ppl, 
+    rf_clf = RandomizedSearchCV(ppl, 
                                 param_distributions=parameters, 
                                 n_iter=30,
                                 random_state=123,
