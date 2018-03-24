7597a84b298c063ab09ff3bb00adc35752c143a1
dryguz <dryguz@gmail.com>
2018-03-24 23:04:41 +0100

    Score for SVM model is 0.824 Score for Logical Regression is 0.8 Score for Random Forest is 0.981

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ Created on Tue Jan 30 14:40:59 2018
 import sys, os
 import numpy as np
 import time
-from sklearn import svm
+from sklearn import svm, linear_model
+from sklearn.ensemble import RandomForestClassifier
 
 path = '/home/mdrygajlo/PycharmProjects/Kaggle_Titanic/'
 sys.path.append(path)
@@ -26,14 +27,26 @@ X, y = cleaner.fit(train)
 
 # set dictionary with models
 models = {'svm': [],
-          'svm_class': [],
           'log_reg': [], 
           'rand_forest': [], 
           'xgboost': []}
 
+#-----------------------------------------------------------------------------
 # first one - svm
+
+svm_parameters = {'Estimator__C': np.linspace(1, 1000, num=50),
+              'Estimator__gamma': np.linspace(0.0001, 0.1, num=50)}
+
+svm_ = svm.SVC(kernel = 'rbf',
+                  random_state=123)
+        
+svm_ = modeling.model_(svm_, svm_parameters)
+
 start = time.time()
-svm_acc, svm_model = modeling.svm_(X, y)
+
+svm_model = svm_.fit(X, y)
+svm_acc = svm_.score(X, y)
+
 duration = time.time() - start
 
 models['svm'].append(np.mean(svm_acc))
@@ -42,48 +55,59 @@ models['svm'].append(svm_model)
 
 print('Score for SVM model is {:.3}'.format(models['svm'][0]))
 
+#-----------------------------------------------------------------------------
 # second one - logical regression
+
+sgd_parameters = {'Estimator__alpha': np.linspace(0.0001, 0.1, 50),
+                  'Estimator__l1_ratio': np.linspace(0, 1, 50)
+                  }
+
+sgd_ = linear_model.SGDClassifier(loss='log', 
+                                     penalty='elasticnet',
+                                     tol=0.001,
+                                     random_state=123)
+
+sgd_ = modeling.model_(sgd_, sgd_parameters)
+
 start = time.time()
-lg_acc, lg_model = modeling.log_reg_(X,y)
+
+sgd_model = sgd_.fit(X, y)
+sgd_acc = sgd_.score(X, y)
+
 duration = time.time() - start
 
-models['log_reg'].append(np.mean(lg_acc))
+models['log_reg'].append(np.mean(sgd_acc))
 models['log_reg'].append(duration)
-models['log_reg'].append(lg_model)
+models['log_reg'].append(sgd_model)
 
 print('Score for Logical Regression is {:.3}'.format(models['log_reg'][0]))
 
+#-----------------------------------------------------------------------------
 #third one - random forest
-#start = time.time()
-#rf_acc, rf_model = modeling.rand_forest_(X,y)
-#duration = time.time() - start
 
-#models['rand_forest_'].append(np.mean(rf_acc))
-#models['rand_forest_'].append(duration)
-#models['rand_forest_'].append(rf_model)
+rf_parameters = {'Estimator__n_estimators': np.linspace(5, 400, 30, dtype='int'),
+                 'Estimator__max_features': np.linspace(0.02, 1.0, num=30)#,
+#                 'Estimator__min_samples_split': np.linspace(2, 10, 1),
+#                 'Estimator__min_samples_leaf': np.linspace(1, 10, 1)
+                 }
 
-#print('Score for Random Forest is {:.3}'.format(models['rand_forest_'][0]))
+rf_model = RandomForestClassifier()
 
-# ----------------------------------------------------------------------------
-# test svm with class
-
-svm_parameters = {'Estimator__C': np.linspace(1, 1000, num=50),
-              'Estimator__gamma': np.linspace(0.0001, 0.1, num=50)}
-
-svm_model = svm.SVC(kernel = 'rbf',
-                  random_state=123)
-        
-_svm_ = modeling.model_(svm_model, svm_parameters)
+rf_model = modeling.model_(rf_model, rf_parameters) 
 
 start = time.time()
 
-_svm_model = _svm_.fit(X, y)
-_svm_acc = _svm_.score(X, y)
+rf_model = rf_model.fit(X, y)
+rf_acc = rf_model.score(X, y)
 
 duration = time.time() - start
 
-models['svm_class'].append(np.mean(_svm_acc))
-models['svm_class'].append(duration)
-models['svm_class'].append(_svm_model)
+models['rand_forest'].append(np.mean(rf_acc))
+models['rand_forest'].append(duration)
+models['rand_forest'].append(rf_model)
 
-print('Score for SVM model is {:.3}'.format(models['svm_class'][0]))
+print('Score for Random Forest is {:.3}'.format(models['rand_forest'][0]))
+
+
+
+
--- modeling.py
+++ modeling.py
@@ -7,12 +7,9 @@ Created on Tue Jan 30 14:58:02 2018
 create classifications model 
 """
 
-from sklearn import svm, linear_model
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV, cross_val_score
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline, FeatureUnion 
-import numpy as np
 import pandas as pd
 
 # ---------------------------------------------------------------------------
@@ -20,15 +17,22 @@ import pandas as pd
 
 
 class model_:
+    """
+    Class model_ takes as parameter a scikit-learn API model 
+    and dictonery with parameters for RandomSearch. 
+    Because of using RS dictonary must have keys following pattern: 
+    {'Estimator__parameter': range_of_values}
+    
+    Class has two methods: fit(X,y) and score(X,y)
+    -fit() returns RandomSearch class model
+    -score() return 5 accuracy values from 5 times fold cross-validation
+    """
     def __init__(self, model, parameters_for_model):
         self.model = model
         self.parameters = parameters_for_model    
         self.for_scalling =[]
         self.X = pd.DataFrame()
         self.y = pd.DataFrame()
-        
-
-        
         
         def select_to_scal(data):
             return data[:,self.for_scalling]
@@ -60,7 +64,9 @@ class model_:
                         random_state=123,                                
                         cv=5)
     
+    
     def fit(self, X, y):
+        
         self.X = X
         self.y = y
         
@@ -73,197 +79,18 @@ class model_:
         
         return self.trained_model
     
+    
     def score(self, X, y):
+        
         self.X = X
         self.y = y
-        
-        
         
         self.models_score = cross_val_score(
                 self.trained_model.best_estimator_, 
                 self.X, 
                 self.y, cv=5)
+        
         return self.models_score
- 
-
-    
+   
 # ---------------------------------------------------------------------------
-# svn_model
-
-def svm_(X, y):
-    """
-    training support vector machines for classification
-    kernel is rbf, optimal C is search for in [1, 10, 100, 1000]
-    """
-    for_scalling =[]
-    
-    for col in ['Age', 'SibSp', 'Parch', 'Fare']:
-        for_scalling.append(X.columns.get_loc(col))
         
-    not_for_scalling = list(set(range(len(X.columns)))-set(for_scalling))
-    
-    
-    def select_to_scal(data):
-        return data[:,for_scalling]
-    
-    
-    def select_not_to_scal(data):
-        return data[:,not_for_scalling]
-    
-    svm_cls = svm.SVC(kernel = 'rbf',
-                      random_state=123)
-    
-    steps = [('Union', FeatureUnion(transformer_list = [
-                ('for_scaler', Pipeline(steps =[
-                        ('select_for_scaler', FunctionTransformer(
-                                select_to_scal)
-                                ),
-                        ('StandardScaler', StandardScaler()
-                                )])
-                ),
-                ('not_scaler', FunctionTransformer(select_not_to_scal))
-                ])
-            ),
-            ('Estimator', svm_cls)
-            ]
-    
-    ppl = Pipeline(steps=steps)
-    
-    parameters = {'Estimator__C': np.linspace(1, 1000, num=50),
-                  'Estimator__gamma': np.linspace(0.0001, 0.1, num=50)}
-    
-    rm_clf = RandomizedSearchCV(ppl, 
-                                param_distributions=parameters, 
-                                n_iter=30,
-                                random_state=123,                                
-                                cv=5)
-
-    svm_model = rm_clf.fit(X, y)
-    svm_acc = cross_val_score(svm_model.best_estimator_, X, y, cv=5)
-    
-    
-    return svm_acc, svm_model
-
-
-# ---------------------------------------------------------------------------
-# log_reg_model
-    
-def log_reg_(X, y):
-    
-    for_scalling =[]
-    
-    for col in ['Age', 'SibSp', 'Parch', 'Fare']:
-        for_scalling.append(X.columns.get_loc(col))
-        
-    not_for_scalling = list(set(range(len(X.columns)))-set(for_scalling))
-    
-    
-    def select_to_scal(data):
-        return data[:,for_scalling]
-    
-    
-    def select_not_to_scal(data):
-        return data[:,not_for_scalling]
-    
-    
-    sgd_cls = linear_model.SGDClassifier(loss='log', 
-                                         penalty='elasticnet',
-                                         tol=0.001,
-                                         random_state=123)
-    
-    
-    steps = [('Union', FeatureUnion(transformer_list = [
-            ('for_scaler', Pipeline(steps =[
-                    ('select_for_scaler', FunctionTransformer(
-                            select_to_scal)
-                            ),
-                    ('StandardScaler', StandardScaler()
-                            )])
-            ),
-            ('not_scaler', FunctionTransformer(select_not_to_scal))
-            ])
-        ),
-        ('Estimator', sgd_cls)
-        ]
-    
-    ppl = Pipeline(steps=steps)
-    
-    
-    parameters = {'Estimator__alpha': np.linspace(0.0001, 0.1, 50),
-                  'Estimator__l1_ratio': np.linspace(0, 1, 50)
-                  }
-    
-    rm_clf = RandomizedSearchCV(ppl, 
-                                param_distributions=parameters, 
-                                n_iter=30,
-                                random_state=123,
-                                cv=5)
-    
-    
-    log_reg_model = rm_clf.fit(X, y)
-    log_reg_acc  =  cross_val_score(log_reg_model.best_estimator_, X, y, cv=5)
-    
-    return log_reg_acc, log_reg_model
-
-
-# ---------------------------------------------------------------------------
-# rand_forest_model
-    
-def rand_forest_(X, y):
-    
-    for_scalling =[]
-    
-    for col in ['Age', 'SibSp', 'Parch', 'Fare']:
-        for_scalling.append(X.columns.get_loc(col))
-        
-    not_for_scalling = list(set(range(len(X.columns)))-set(for_scalling))
-    
-    
-    def select_to_scal(data):
-        return data[:,for_scalling]
-    
-    def select_not_to_scal(data):
-        return data[:,not_for_scalling]
-    
-    
-    rf_clf = RandomForestClassifier(random_state=123
-                                    
-                                    )
-
-    steps = [('Union', FeatureUnion(transformer_list = [
-            ('for_scaler', Pipeline(steps =[
-                    ('select_for_scaler', FunctionTransformer(
-                            select_to_scal)
-                            ),
-                    ('StandardScaler', StandardScaler()
-                            )])
-            ),
-            ('not_scaler', FunctionTransformer(select_not_to_scal))
-            ])
-        ),
-        ('Estimator', rf_clf)
-        ]
-    
-    ppl = Pipeline(steps=steps)
-
-
-    parameters = {'Estimator__n_estimators': np.linspace(3, 100, 3),
-                  'Estimator__': '',
-                  'Estimator__': ''
-            }
-    
-    rf_clf = RandomizedSearchCV(ppl, 
-                                param_distributions=parameters, 
-                                n_iter=30,
-                                random_state=123,
-                                cv=5)
-    
-    rand_forest_model, rand_forest_model = 1, 1
-    return rand_forest_model, rand_forest_model
-
-# ---------------------------------------------------------------------------
-# xgboost_model
-    
-def xgboost_(df):
-    xgb_acc, xgb_model = 1, 1
-    return xgb_acc, xgb_model
